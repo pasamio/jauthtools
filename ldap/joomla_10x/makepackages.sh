c27e548e3db560ff94bfeb18cf05d27258394e3e
pasamio <pasamio@e1d38f20-662c-0410-abca-cf9e5fa40de9>
2006-12-24 16:05:03 +0000

    Lets try this again

--- ldap/joomla_10x/makepackages.sh
+++ ldap/joomla_10x/makepackages.sh
@@ -1,13 +1,20 @@
 #!/bin/sh
 # Builds the packages
 ORIGINAL=`pwd`
+FILELIST=''
 for i in `ls trunk`
 do
 	echo Building $i
 	cd trunk/$i
 	tar -zcvf ../../packages/$i.tgz *
 	echo
+	FILELIST="$FILELIST $i.tgz "
 	cd $ORIGINAL
 done
+
+cd packages
+DT=`date +%Y%m%d`
+echo Building Full Package
+tar -zcvf ldappack_joomla10_$DT.tgz $FILELIST
 
 echo Done.
