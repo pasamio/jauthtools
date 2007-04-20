dfcab02bfb134487eeca147617c42d0ba0725954
pasamio <pasamio@e1d38f20-662c-0410-abca-cf9e5fa40de9>
2007-04-20 05:22:46 +0000

    Altered a few settings

--- sso/joomla10x/trunk/httpsso.php
+++ sso/joomla10x/trunk/httpsso.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * HTTP Auth Mambot
  * 
@@ -20,27 +21,28 @@
 // no direct access
 defined('_VALID_MOS') or die('Restricted access');
 
-if(!function_exists('addLogEntry')) {
+if (!function_exists('addLogEntry')) {
 	function addLogEntry($application, $type, $priority, $message) {
-		if(defined('_JLOGGER_API')) {
+		if (defined('_JLOGGER_API')) {
 			global $database;
 			$logentry = new JLogEntry($database);
 			$logentry->application = $application;
-			$logentry->type 		= $type;
-			$logentry->priority 	= $priority;
-			$logentry->message 	= $message;
+			$logentry->type = $type;
+			$logentry->priority = $priority;
+			$logentry->message = $message;
 			$logentry->store() or die('Log entry save failed');
 		}
 	}
 }
 
-if(!function_exists('ldap_connect')) {
-	addLogEntry('HTTP SSO Mambot', 'authentication','crit','PHP LDAP Library not detected');
-} else if(!class_exists('ldapConnector')) {
-	addLogEntry('HTTP SSO Mambot', 'authentication','crit','Joomla! LDAP Library not detected');
-} else {
-	$_MAMBOTS->registerFunction('onAfterStart', 'botDoHTTPSSOLogin');
-}
+if (!function_exists('ldap_connect')) {
+	addLogEntry('HTTP SSO Mambot', 'authentication', 'crit', 'PHP LDAP Library not detected');
+} else
+	if (!class_exists('ldapConnector')) {
+		addLogEntry('HTTP SSO Mambot', 'authentication', 'crit', 'Joomla! LDAP Library not detected');
+	} else {
+		$_MAMBOTS->registerFunction('onAfterStart', 'botDoHTTPSSOLogin');
+	}
 
 /**
  * Initiates a login 
@@ -54,97 +56,96 @@ function botDoHTTPSSOLogin() {
 	$query = "SELECT params FROM #__mambots WHERE element = 'httpsso' AND folder = 'system'";
 	$database->setQuery($query);
 	$params = $database->loadResult();
-	$mambotParams =& new mosParameters( $params );
+	$mambotParams = & new mosParameters($params);
 	$remoteuser = $mambotParams->get('userkey');
 	$username = mosGetParam($_SERVER, $remoteuser, null);
 	if ($username != NULL) {
-			// Has a remote_user field set, get the username and attempt to sign them in.
-			$replacement = $mambotParams->get('username_replacement');
-			foreach(explode('|',  $mambotParams->get('username_replacement')) as $replace) {
-				$username = str_replace($replace,'',$username);
-			}
+		// Has a remote_user field set, get the username and attempt to sign them in.
+		$replacement = $mambotParams->get('username_replacement');
+		foreach (explode('|', $mambotParams->get('username_replacement')) as $replace) {
+			$username = str_replace($replace, '', $username);
+		}
 
-			//load user bot group
-			$query = 'SELECT id FROM #__users WHERE username=' . $database->Quote($username);
-			$database->setQuery($query);
-			$userId = intval($database->loadResult());
-			$user = new mosUser($database);
-			if ($userId < 1) {
+		//load user bot group
+		$query = 'SELECT id FROM #__users WHERE username=' . $database->Quote($username);
+		$database->setQuery($query);
+		$userId = intval($database->loadResult());
+		$user = new mosUser($database);
+		if ($userId < 1) {
+			if ($mambotParams->get('useglobal')) {
 				$ldap = new ldapConnector();
-				$ldap->connect();
-				$ldap->bind();
-				if(intval($mambotParams->get('autocreate'))) {
-					// Create user 
-					$user->username = $username;
-					$ldap->populateUser($user, $mambotParams->get('groupMap'));
-					$user->id = 0;
-					$row->registerDate 	= date( 'Y-m-d H:i:s' );
-					if($user->usertype == 'Public Frontend' && !$mambotParams->get('autocreateregistered')) {
-						addLogEntry('Kerberos SSO Mambot', 'authentication', 'notice', 'User creation halted for '. $username .' since they would only be registered');
-						//$ldap->close();
-						return false;
-					} else {
-						if(!$user->store()) {
-							addLogEntry('Kerberos SSO Mambot', 'authentication', 'err', 'Could not autocreate user:'. print_r($user,1));
-						}
+			} else {
+				$ldap = new ldapConnector($mambotParams);
+			}
+			$ldap->connect();
+			$ldap->bind();
+			if (intval($mambotParams->get('autocreate'))) {
+				// Create user 
+				$user->username = $username;
+				$ldap->populateUser($user, $mambotParams->get('groupMap'));
+				$user->id = 0;
+				$row->registerDate = date('Y-m-d H:i:s');
+				if ($user->usertype == 'Public Frontend' && !$mambotParams->get('autocreateregistered')) {
+					addLogEntry('HTTP SSO Mambot', 'authentication', 'notice', 'User creation halted for ' . $username . ' since they would only be registered');
+					//$ldap->close();
+					return false;
+				} else {
+					if (!$user->store()) {
+						addLogEntry('HTTP SSO Mambot', 'authentication', 'err', 'Could not autocreate user:' . print_r($user, 1));
 					}
 				}
-			} else if($userId) {
+			}
+		} else
+			if ($userId) {
 				$user->load(intval($userId));
 			} else {
 				//$ldap->close();
 				return false;
 			}
-				
-			// check to see if user is blocked from logging in (ignored)
-			if ($user->block == 1) {
-				//$ldap->close();
-				return false;
-			}
-			// fudge the group stuff
-			$grp = $acl->getAroGroup($user->id);
-			$user->gid = 1;
 
-			if ($acl->is_group_child_of($grp->name, 'Registered', 'ARO') || $acl->is_group_child_of($grp->name, 'Public Backend', 'ARO')) {
-				// fudge Authors, Editors, Publishers and Super Administrators into the Special Group
-				$user->gid = 2;
-			}
-			$user->usertype = $grp->name;
+		// check to see if user is blocked from logging in (ignored)
+		if ($user->block == 1) {
+			//$ldap->close();
+			return false;
+		}
+		// fudge the group stuff
+		$grp = $acl->getAroGroup($user->id);
+		$user->gid = 1;
 
-			$session = & $mainframe->_session;
-			$session->guest = 0;
-			$session->username = $user->username;
-			$session->userid = intval($user->id);
-			$session->usertype = $user->usertype;
-			$session->gid = intval($user->gid);
-			$userid = $user->id;
-			// Persistence
-			$query = "SELECT id, name, username, password, usertype, block, gid"
-			. "\n FROM #__users"
-			. "\n WHERE id = $userid"
-			;
-			$row = null;
-			$database->setQuery( $query );
-			$database->loadObject($row);
-			$lifetime               = time() + 365*24*60*60;
-			$remCookieName  = mosMainFrame::remCookieName_User();
-			$remCookieValue = mosMainFrame::remCookieValue_User( $row->username ) . mosMainFrame::remCookieValue_Pass( $row->password ) . $row->id;
-			setcookie( $remCookieName, $remCookieValue, $lifetime, '/' );
-			$session->store();
-			// update user visit data
-			$currentDate = date("Y-m-d\TH:i:s");
+		if ($acl->is_group_child_of($grp->name, 'Registered', 'ARO') || $acl->is_group_child_of($grp->name, 'Public Backend', 'ARO')) {
+			// fudge Authors, Editors, Publishers and Super Administrators into the Special Group
+			$user->gid = 2;
+		}
+		$user->usertype = $grp->name;
 
-			$query = "UPDATE #__users"
-			. "\n SET lastvisitDate = ". $database->Quote( $currentDate )
-			. "\n WHERE id = " . (int) $session->userid
-			;
-			$database->setQuery($query);
-			$database->Query();
-			
-			mosCache :: cleanCache();
-			return true;
+		$session = & $mainframe->_session;
+		$session->guest = 0;
+		$session->username = $user->username;
+		$session->userid = intval($user->id);
+		$session->usertype = $user->usertype;
+		$session->gid = intval($user->gid);
+		$userid = $user->id;
+		// Persistence
+		$query = "SELECT id, name, username, password, usertype, block, gid" . "\n FROM #__users" . "\n WHERE id = $userid";
+		$row = null;
+		$database->setQuery($query);
+		$database->loadObject($row);
+		$lifetime = time() + 365 * 24 * 60 * 60;
+		$remCookieName = mosMainFrame :: remCookieName_User();
+		$remCookieValue = mosMainFrame :: remCookieValue_User($row->username) . mosMainFrame :: remCookieValue_Pass($row->password) . $row->id;
+		setcookie($remCookieName, $remCookieValue, $lifetime, '/');
+		$session->store();
+		// update user visit data
+		$currentDate = date("Y-m-d\TH:i:s");
+
+		$query = "UPDATE #__users" . "\n SET lastvisitDate = " . $database->Quote($currentDate) . "\n WHERE id = " . (int) $session->userid;
+		$database->setQuery($query);
+		$database->Query();
+
+		mosCache :: cleanCache();
+		return true;
 	} else {
 		return false;
-	}	
+	}
 }
 ?>
