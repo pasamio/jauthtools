8aba9ada2bc31ca78f7ac9f25e991d7ac2877392
Sam Moffatt <pasamio@users.noreply.github.com>
2007-06-19 06:10:37 +0000

    Fixed up minor fault in loop logic

--- ldap/joomla_10x/trunk/joomla_ldap.bot/joomla.ldap.php
+++ ldap/joomla_10x/trunk/joomla_ldap.bot/joomla.ldap.php
@@ -247,7 +247,7 @@ class ldapConnector {
 		$search_result = ldap_search($resource, $dn, $filter);
 		addLogEntry('LDAP Library', 'search', 'debug', 'Search for '. $filter . ' in '. $dn . ' returned ' . $search_result . ' with '. ldap_count_entries($resource, $search_result) .' results');
 		if ($search_result && ($count = ldap_count_entries($resource, $search_result)) > 0) {
-			for ($i = 0; $i == $count-1; $i++) {
+			for ($i = 0; $i < $count-1; $i++) {
 				$attributes[$i] = Array ();
 				if (!$i) {
 					$firstentry = ldap_first_entry($resource, $search_result);
