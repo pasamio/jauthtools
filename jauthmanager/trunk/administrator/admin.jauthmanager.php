5d0bf37792110177f8d6dabb45a8d2f5a16388c4
pasamio <pasamio@e1d38f20-662c-0410-abca-cf9e5fa40de9>
2007-07-26 06:13:31 +0000

    Initial import.

---
+++ jauthmanager/trunk/administrator/admin.jauthmanager.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * JAuthTools Manager for 1.5
+ * 
+ * This component provides support tools for JAuthTools and 1.5
+ * 
+ * PHP4/5
+ *  
+ * Created on 26 Jul 2007
+ * 
+ * @package    JAuthTools
+ * @subpackage Manager
+ * @author Sam Moffatt <S.Moffatt@toowoomba.qld.gov.au>
+ * @author Toowoomba City Council Information Management Branch
+ * @license GNU/GPL http://www.gnu.org/licenses/gpl.html
+ * @copyright 2007 Toowoomba City Council/Sam Moffatt 
+ * @version SVN: $Id:$
+ * @see JoomlaCode Project: http://joomlacode.org/gf/project/
+ */
+
+// no direct access
+
+defined( '_JEXEC' ) or die( 'Restricted access' );
+
+// Require the base controller
+
+require_once( JPATH_COMPONENT.DS.'controller.php' );
+
+// Require specific controller if requested
+if($controller = JRequest::getWord('controller')) {
+    $path = JPATH_COMPONENT.DS.'controllers'.DS.$controller.'.php';
+    if (file_exists($path)) {
+        require_once $path;
+    } else {
+        $controller = '';
+    }
+}
+
+// Create the controller
+$classname    = 'JAuthController'.$controller;
+$controller   = new $classname( );
+
+// Perform the Request task
+$controller->execute( JRequest::getVar( 'task' ) );
+
+// Redirect if set by the controller
+$controller->redirect();
+ 
+?>
---
+++ jauthmanager/trunk/administrator/controller.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Document Description
+ * 
+ * Document Long Description 
+ * 
+ * PHP4/5
+ *  
+ * Created on 26 Jul 2007
+ * 
+ * @package JAuthTools
+ * @subpackage Manager
+ * @author Sam Moffatt <author@toowoomba.qld.gov.au>
+ * @author Toowoomba City Council Information Management Branch
+ * @license GNU/GPL http://www.gnu.org/licenses/gpl.html
+ * @copyright 2007 Toowoomba City Council/Sam Moffatt
+ * @version SVN: $Id:$
+ * @see JoomlaCode Project: http://joomlacode.org/gf/project/
+ */
+ 
+// no direct access
+
+defined( '_JEXEC' ) or die( 'Restricted access' );
+
+jimport('joomla.application.component.controller');
+
+/**
+ * JAuth Manager Component Controller
+ *
+ * @package    Joomla.Tutorials
+ * @subpackage Components
+ */
+class JAuthController extends JController
+{
+    /**
+     * Method to display the view
+     *
+     * @access    public
+     */
+    function display()
+    {
+        parent::display();
+    }
+
+}
+
+?>
